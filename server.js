9b2fc0e2001f1055f4ba5130834c25a7645f266d
YelianaC <yelianacampo@gmail.com>
2025-11-12 20:56:36 -0500

    Se agrega el proyeto

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from 'express'
+import mongoose from 'mongoose'
+import cors from 'cors'
+
+const app = express()
+
+//configurando nuestro servidor para permitir
+app.use(cors())
+app.use(express.json())
+
+const MONGO_URI = 'jacobogarcesoquendo:aFJzVMGN3o7fA38A@cluster0.mqwbn.mongodb.net/{yelianacampobarraza}'
+
+mongoose.connect(MONGO_URI)
+    .then(() => console.log('Conectado a Atlas'))
+    .catch(err => console.log('Error de conexión', err))
+
+const PORT = 5000
+app.listen(PORT, () => console.log('Servidor escuchando en puerto 5000'))
