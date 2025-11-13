a948bd209cbebad11e8deb4e973a1cc2bdc80a3e
YelianaC <yelianacampo@gmail.com>
2025-11-12 22:13:16 -0500

    guardando avances

--- models/Tarea.js
+++ models/Tarea.js
@@ -1,3 +1,9 @@
 import mongoose from 'mongoose';
 
-const tareaSchone = 
+const tareaSchena = new mongoose.Schena({
+    titulo:{type:String, require: true},
+    descripcion: {type:String},
+    fechaCreacion: {type:Date, default:Date.now}
+})
+
+export default mongoose.model('Tarea', tareaSchema);
